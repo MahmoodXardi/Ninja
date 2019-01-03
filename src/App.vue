9090b6b8c2460f86573073fae8a1871d17ded98e
MahmoodXardi <41924380+MahmoodXardi@users.noreply.github.com>
2019-01-04 01:45:17 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div><appHeader></appHeader> <ninjas></ninjas> <appFooter></appFooter></div>
+</template>
+
+<script>
+import AppHeader from "./components/Header";
+import AppFooter from "./components/Footer";
+import Ninjas from "./components/Ninjas";
+
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+    AppFooter,
+    Ninjas
+  }
+};
+</script>
+
+<style scoped></style>
